cc1e5068fb92397e207b942144800572d0109564
village-coders <villagecoders7@gmail.com>
2026-01-11 17:43:42 +0100

    lksjdjkshfdf

--- Controllers/newsController.js
+++ Controllers/newsController.js
@@ -11,7 +11,7 @@ const createNews = async (req, res, next) => {
 
     const news = await newsModel.create({
       ...req.body,
-      mainImage: req.files.mainImage[0].path.replace(/\\/g, '/'),
+      mainImage: req.files.mainImage[0]?.path.replace(/\\/g, '/'),
       image1: req.files.image1?.[0]?.path.replace(/\\/g, '/'),
       image2: req.files.image2?.[0]?.path.replace(/\\/g, '/'),
       image3: req.files.image3?.[0]?.path.replace(/\\/g, '/'),
